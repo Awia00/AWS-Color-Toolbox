ae02d18528930c09297b9e296b2d546dd6b6aeb8
Anders Wind Steffensen <awia00@gmail.com>
2014-11-03 18:15:50 +0100

    Made Intensify safe for negative values. Updated release dll

--- C# Toolboxes/Toolboxes/Color Toolbox/ColorToolbox.cs
+++ C# Toolboxes/Toolboxes/Color Toolbox/ColorToolbox.cs
@@ -34,9 +34,9 @@ namespace Color_Toolbox
         /// <returns></returns>
         public static Color ColorIntensify(Color color1, double intensity)
         {
-            int red = (int)Math.Min(255, color1.R * (intensity));
-            int green = (int)Math.Min(255, color1.G * (intensity));
-            int blue = (int)Math.Min(255, color1.B * (intensity));
+            int red = (int)Math.Max(Math.Min(255, color1.R * (intensity)),0);
+            int green = (int)Math.Max(Math.Min(255, color1.G * (intensity)),0);
+            int blue = (int)Math.Max(Math.Min(255, color1.B * (intensity)), 0);
             return Color.FromArgb(red, green, blue);
         }
 
